ea489371b159eea151005bf605dab26106b9138a
kudkud32 <yudha@authscure.com.my>
2019-11-04 20:57:52 +0700

    Initial Commit

---
+++ basiccalculator/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  basiccalculator
+//
+//  Created by Yudha Pratama Putra on 04/11/19.
+//  Copyright © 2019 Yudha Pratama Putra. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
